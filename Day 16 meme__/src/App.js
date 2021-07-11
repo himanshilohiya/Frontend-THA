5392217c46e263421c977e33ae791dadd267bfd3
springspoon <84191914+springspoon@users.noreply.github.com>
2021-07-12 00:22:18 +0530

    Add files via upload

---
+++ Day 16 meme__/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img class="image" src="https://static.toiimg.com/photo/74674393.cms" alt="meme"></img>
+      </div>
+    
+  );
+}
+
+export default App;
